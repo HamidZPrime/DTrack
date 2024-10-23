d6ca46653bb9ad9bd34e3308bf82c25d59b37f4b
HamidZPrime <hheyhat@zprime.ai>
2024-10-24 00:21:32 +0100

    fixing qr code image

--- dtrack/settings.py
+++ dtrack/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
