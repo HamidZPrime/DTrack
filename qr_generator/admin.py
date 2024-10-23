c82ba7c74b0533689e51cee92a94b53604ffad0b
HamidZprime <hheyhat@zprime.ai>
2024-10-23 13:35:41 +0100

    Resolved merge conflicts with Heroku main branch

--- qr_generator/admin.py
+++ qr_generator/admin.py
@@ -11,11 +11,7 @@ class SupplierQRAdmin(admin.ModelAdmin):
     """
     list_display = ('supplier', 'qr_token', 'get_qr_code_image', 'created_at')
     search_fields = ('supplier__email', 'supplier__first_name', 'supplier__last_name', 'qr_token')
-<<<<<<< HEAD
     readonly_fields = ('qr_code_image', 'qr_token', 'get_qr_code_image', 'created_at')
-=======
-    readonly_fields = ('qr_token', 'get_qr_code_image', 'created_at')
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
 
     def get_qr_code_image(self, obj):
         """
@@ -26,7 +22,6 @@ class SupplierQRAdmin(admin.ModelAdmin):
         return _("No QR Code")
     get_qr_code_image.short_description = _("QR Code Image")
 
-<<<<<<< HEAD
     def save_model(self, request, obj, form, change):
         """
         Override save_model to automatically generate QR code image upon save.
@@ -34,8 +29,6 @@ class SupplierQRAdmin(admin.ModelAdmin):
         obj.generate_qr_code()
         super().save_model(request, obj, form, change)
 
-=======
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
 
 @admin.register(ProductQR)
 class ProductQRAdmin(admin.ModelAdmin):
@@ -45,11 +38,7 @@ class ProductQRAdmin(admin.ModelAdmin):
     """
     list_display = ('supplier', 'product_id', 'qr_token', 'get_qr_code_image', 'created_at')
     search_fields = ('supplier__email', 'supplier__first_name', 'supplier__last_name', 'product_id', 'qr_token')
-<<<<<<< HEAD
     readonly_fields = ('qr_code_image', 'qr_token', 'get_qr_code_image', 'created_at')
-=======
-    readonly_fields = ('qr_token', 'get_qr_code_image', 'created_at')
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
 
     def get_qr_code_image(self, obj):
         """
@@ -60,7 +49,6 @@ class ProductQRAdmin(admin.ModelAdmin):
         return _("No QR Code")
     get_qr_code_image.short_description = _("QR Code Image")
 
-<<<<<<< HEAD
     def save_model(self, request, obj, form, change):
         """
         Override save_model to automatically generate QR code image upon save.
@@ -68,8 +56,6 @@ class ProductQRAdmin(admin.ModelAdmin):
         obj.generate_qr_code()
         super().save_model(request, obj, form, change)
 
-=======
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
 
 @admin.register(CertificateQR)
 class CertificateQRAdmin(admin.ModelAdmin):
@@ -79,11 +65,7 @@ class CertificateQRAdmin(admin.ModelAdmin):
     """
     list_display = ('supplier', 'certificate_id', 'qr_token', 'get_qr_code_image', 'created_at')
     search_fields = ('supplier__email', 'supplier__first_name', 'supplier__last_name', 'certificate_id', 'qr_token')
-<<<<<<< HEAD
     readonly_fields = ('qr_code_image', 'qr_token', 'get_qr_code_image', 'created_at')
-=======
-    readonly_fields = ('qr_token', 'get_qr_code_image', 'created_at')
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
 
     def get_qr_code_image(self, obj):
         """
@@ -93,7 +75,6 @@ class CertificateQRAdmin(admin.ModelAdmin):
             return format_html(f'<img src="{obj.qr_code_image.url}" width="100" height="100" />')
         return _("No QR Code")
     get_qr_code_image.short_description = _("QR Code Image")
-<<<<<<< HEAD
 
     def save_model(self, request, obj, form, change):
         """
@@ -102,5 +83,3 @@ class CertificateQRAdmin(admin.ModelAdmin):
         obj.generate_qr_code()
         super().save_model(request, obj, form, change)
 
-=======
->>>>>>> fbe776ad210208b47bf216e2553c92919cdbdad8
