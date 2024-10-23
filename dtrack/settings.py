e42c1a403f6076150f10d645a271a5eb4d9c46f8
HamidZprime <hheyhat@zprime.ai>
2024-10-23 11:43:56 +0100

    Updated database configurations and environment variables for Heroku

--- dtrack/settings.py
+++ dtrack/settings.py
@@ -85,15 +85,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dtrack.wsgi.application'
 
-# Database configuration without dj_database_url
+# Database configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
+        'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT', default='5432'),
+        'CONN_MAX_AGE': 600,  # Keep connections open for a reasonable time for performance
+        'OPTIONS': {
+            'sslmode': 'require',  # Ensure SSL is enabled
+        }
     }
 }
 
