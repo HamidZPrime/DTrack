1aa99813d58e668a9810d2474ce3aba3c172c721
HamidZPrime <hheyhat@zprime.ai>
2024-10-24 20:49:43 +0100

    Setting Update

--- dtrack/settings.py
+++ dtrack/settings.py
@@ -123,6 +123,11 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Static Files
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
+# Add this line to set the directory where collectstatic will store static files locally before uploading to S3.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media Files
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Email Configuration using Amazon SES
