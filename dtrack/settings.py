3bec5784e96f235ed95f511085d884f57ebe1677
HamidZPrime <hheyhat@zprime.ai>
2024-10-24 20:38:46 +0100

    Update Core

--- dtrack/settings.py
+++ dtrack/settings.py
@@ -128,12 +128,12 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Email Configuration using Amazon SES
 EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = env("EMAIL_HOST", default="email-smtp.us-east-1.amazonaws.com")
+EMAIL_HOST = env("EMAIL_HOST", default="email-smtp.eu-west-1.amazonaws.com")
 EMAIL_PORT = env.int("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="webmaster@dtrack.zprime.ai")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="wno-reply@zprimedev.com")
 
 # TWILIO VOIP
 TWILIO_ACCOUNT_SID = env("TWILIO_ACCOUNT_SID")
